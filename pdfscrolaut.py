dcfdf949daf62d242d013af59340a95899623241
Guravjayesh <88273868+Guravjayesh@users.noreply.github.com>
2021-09-06 21:08:31 +0530

    Add files via upload

---
+++ pdfscrolaut.py
@@ -0,0 +1,27 @@
+from cvzone.HandTrackingModule import HandDetector
+import cv2
+import pyautogui
+from time import sleep
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+detector = HandDetector(detectionCon=0.8, maxHands=2)
+
+while True:
+    success, img = cap.read()
+
+    img = detector.findHands(img)
+    lmList, bboxInfo = detector.findPosition(img)
+    if lmList:
+        bbox = bboxInfo['bbox']
+        myHandType = detector.handType()
+        cv2.putText(img, f'Hand:{myHandType}', (bbox[0], bbox[1] - 30),
+                    cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+
+
+    cv2.imshow("Image", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
